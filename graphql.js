f946bbd529e2d8e8025325cc296bf2b3c28160ef
mmykhaylov <mikemikhailov2003@gmail.com>
2021-01-16 19:52:15 +0200

    Added JWT to Apollo context

--- graphql.js
+++ graphql.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-param-reassign */
 require('dotenv').config({ path: './variables.env' });
 const { ApolloServer } = require('apollo-server-lambda');
+const { default: jwtVerify } = require('jose/jwt/verify');
+const { default: createRemoteJWKSet } = require('jose/jwks/remote');
 
 const middy = require('@middy/core');
 const connectToDatabase = require('./db');
@@ -23,6 +25,24 @@ const server = new ApolloServer({
   dataSources: () => ({
     mongodbAPI: MongodbAPI,
   }),
+  context: async ({ event: req }) => {
+    if (req.headers.Authorization) {
+      const token = req.headers.Authorization.split(' ')[1] || '';
+      const JWKS = createRemoteJWKSet(
+        new URL('https://dev-q6a92igd.eu.auth0.com/.well-known/jwks.json'),
+      );
+      try {
+        const { payload, protectedHeader } = await jwtVerify(token, JWKS, {
+          issuer: 'https://dev-q6a92igd.eu.auth0.com/',
+          audience: 'https://u06740719i.execute-api.eu-central-1.amazonaws.com/dev/graphql',
+        });
+        return { payload, protectedHeader };
+      } catch (err) {
+        return {};
+      }
+    }
+    return {};
+  },
 });
 const graphqlHandler = server.createHandler({
   cors: {
