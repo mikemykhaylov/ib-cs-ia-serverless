0c6d59edf50001364aca2ed711d1830fa2df9d23
mmykhaylov <mikemikhailov2003@gmail.com>
2021-02-22 19:49:47 +0100

    Removed unused eslint rule

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable import/no-unresolved */
 import middy from '@middy/core';
 import { ApolloServer } from 'apollo-server-lambda';
